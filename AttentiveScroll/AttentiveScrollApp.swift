2cc8436ea056695114f87f1c855a8491b8dc7852
Maxim Keegan <maxim@keegan.ru>
2023-10-02 20:25:14 +0500

    Initial Commit

---
+++ AttentiveScroll/AttentiveScrollApp.swift
@@ -0,0 +1,17 @@
+//
+//  AttentiveScrollApp.swift
+//  AttentiveScroll
+//
+//  Created by Maxim Keegan on 02.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct AttentiveScrollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
