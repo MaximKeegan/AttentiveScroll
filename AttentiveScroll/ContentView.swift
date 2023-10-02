a35031539582c0906b3a21f913b965313f10d96c
Maxim Keegan <maxim@keegan.ru>
2023-10-02 21:25:44 +0500

    Commit

--- AttentiveScroll/ContentView.swift
+++ AttentiveScroll/ContentView.swift
@@ -9,13 +9,24 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView {
+            ZStack {
+                ScrollView {
+                        Text("test\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest")
+                            .frame(maxWidth: .infinity, maxHeight: .infinity)
+                }
+                .safeAreaInset(edge: .bottom) {
+                    Rectangle()
+                        .fill(Color.red.opacity(0.8))
+                        .frame(width: .infinity, height: 50)
+                }
+            }
+            .tabItem {
+                Image(systemName: "star.fill")
+                Text("First")
+            }
+            
         }
-        .padding()
     }
 }
 
